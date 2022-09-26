adb21e8f2e64a0fb82da9b01672dfa7593605f0f
luiggisales <luiggi.sales.silva07@aluno.ifce.edu.br>
2022-09-26 19:21:51 -0300

    alteracoes visuais no jogo

--- src/dama/Tabuleiro.js
+++ src/dama/Tabuleiro.js
@@ -18,15 +18,31 @@ class Tabuleiro extends React.Component
                 [0, 2, 0, 2, 0, 2, 0, 2],
                 [2, 0, 2, 0, 2, 0, 2, 0]
             ],
+            vez: 'Jogue',
             criar_tabuleiro: props.criarTabuleiro
         }
         this.novo_tabuleiro = this.state.tabuleiro
+        this.clearAll = this.clearAll.bind(this)
     }
-
+    clearAll(){
+        localStorage.clear()
+        window.location.reload()
+    }
     render() {
         let novo = this.props.criarTabuleiro(this.state.tabuleiro)
+        let vez = JSON.parse(localStorage.getItem('dama_antiga'))
+        const colorPlayer = (vez) =>{
+            let color = ""
+
+            if (vez === 1 ){ color = "azul"}
+            else if (vez === 2 ){color = "vermelho"}
+
+            return color
+        }
+        
         this.setState({
-            tabuleiro: novo ?? this.state.tabuleiro
+            tabuleiro: novo ?? this.state.tabuleiro,
+            vez: vez ?? this.state.vez,
         })
 
         let x = 0, y = 0;
@@ -41,7 +57,15 @@ class Tabuleiro extends React.Component
             return <div className="row">{r}</div>
         });
         return (
-            <div className="tabuleiro">{rows}</div>
+            <div className="tabuleiro">
+                {rows}
+                <div className='area-buttons'>
+                    <button className='reniciar' onClick={this.clearAll}>Reniciar</button>
+                    <span>
+                        <h3 className={colorPlayer(this.state.vez)}>{this.state.vez === 1 ? 'Vez do jogador azul': this.state.vez === 2 ? 'Vez do jogador vermelho' : this.state.vez }</h3>
+                    </span>
+                </div>
+            </div>
         )
     }
 }
